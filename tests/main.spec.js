af448727fce7aa5fd12dfb9e1808ca15a9af909a
Aldo Soares <soares_aldo@hotmail.com>
2018-03-29 22:52:04 -0300

    [IMP] Implementação de teste para testar execução de script node

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,52 +1,15 @@
-const expect = require('chai').expect;
+import { expect } from 'chai';
 
-describe('Main', () => {
-  let arr;
+const exec = require('child_process').exec;
+const btcConverter = 'node ./src/main.js';
 
-  // roda uma vez antes do bloco
-  before( () => {
-    // inicia uma conexão no banco
-    // criar um conjunto de dados
+describe('Main CLI', () => {
+  it('should return Hello World', (done) => {
+    exec(btcConverter, (err, stdout, stderr) => {
+      console.log(err);
+      if(err) throw err;
+      expect(stdout.replace('\n' ,'')).to.be.equal('Hello World!');
+      done();
+    });
   });
-
-  // roda uma vez, depois do bloco
-  after( () => {
-    // fecha a conexão do banco
-    // apagar esse conjunto de dados
-  });
-
-  // roda todas as vezes, antes de CADA bloco
-  beforeEach( () => {
-    arr = [1, 2, 3];
-  });
-
-  // roda todas as vezes, depois de CADA bloco
-  afterEach( () => {
-
-  });
-
-  // testar tipos ou se existe (smoke test)
-  it('should be an array', () => {
-    expect(arr).to.be.a('array');
-  });
-
-  it('should return true when pop in the array', () => {
-    expect(arr.pop() === 3).to.be.true;
-  });
-
-  it('should have a size of 4 when push another value to the array', () => {
-    arr.push(4);
-    expect(arr).to.have.lengthOf(4); // 4
-  });
-
-  it('should remove the value 3 when use pop in the array', () => {
-    arr.pop();
-    expect(arr).to.not.include(3);
-  });
-
-  it('should remove the value 3 when use pop in the array', () => {
-    arr.pop();
-    expect(arr).to.have.lengthOf(2); // 2
-  });
-
 });
