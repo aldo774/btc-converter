977ec531a7cfbb30f4261895fdd819db111919a0
Aldo Soares <soares_aldo@hotmail.com>
2018-03-30 02:30:10 -0300

    [IMP] Inclusão de loader nas requisições através do 'ora'

--- bin/ConvertBTC.js
+++ bin/ConvertBTC.js
@@ -1,7 +1,13 @@
 'use strict';
 
-var request = require('request');
 var chalk = require('chalk');
+var request = require('request');
+var ora = require('ora');
+
+var spinner = ora({
+  text: 'Retrieving Bitcoin data...',
+  color: 'yellow'
+});
 
 function convertBTC() {
   var currency = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'USD';
@@ -9,8 +15,11 @@ function convertBTC() {
 
   var url = 'https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=' + currency + '&amount=' + amount;
 
+  spinner.start();
+
   request(url, function (error, response, body) {
     var apiResponse = void 0;
+    spinner.stop();
     try {
       apiResponse = JSON.parse(body);
     } catch (parseError) {
@@ -18,8 +27,8 @@ function convertBTC() {
       return parseError;
     }
 
-    console.log(chalk.red(amount) + ' BTC to ' + chalk.cyan(currency) + ' = ' + chalk.yellow(apiResponse.price));
+    return console.log(chalk.red(amount) + ' BTC to ' + chalk.cyan(currency) + ' = ' + chalk.yellow(apiResponse.price));
   });
-};
+}
 
 module.exports = convertBTC;
